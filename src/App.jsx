d44c16c3eb414e304fc4502913d2feb9c440bec9
BalavigneshRajasekar <you@example.com>
2024-09-20 19:59:01 +0530

    Project setuped

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <h1>Personal portfolio</h1>
+    </>
+  );
+}
+
+export default App;
